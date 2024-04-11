2f8548b8769c4e1998bfc6423b30b32a940ec6f8
OlaOluwalekan <olalekanbello534@gmail.com>
2024-04-11 09:04:52 +0100

    initial commit

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
-import AuthProvider from "./(components)/AuthProvider";
-import Nav from "./(components)/Nav";
+import { Inter } from "next/font/google";
 import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -10,12 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <AuthProvider>
-        <body className="bg-gray-100">
-          <Nav />
-          <div className="m-2">{children}</div>
-        </body>
-      </AuthProvider>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
